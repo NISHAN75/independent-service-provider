cf423a0ef4fd2a60133d202c29de6f9c767a054c
nishandas <nishandas880@gmail.com>
2022-04-17 13:36:11 +0600

    banner area finshed

--- src/component/Page/Home/Banner/Banner.js
+++ src/component/Page/Home/Banner/Banner.js
@@ -1,46 +1,69 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
+import banner1 from "../../../../img/banner/banner1.png";
+import banner2 from "../../../../img/banner/banner2.png";
+import banner3 from "../../../../img/banner/banner3.png";
+import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div className="mt">
+    <div className="slider-area">
       <Carousel fade>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://images.pexels.com/photos/3184405/pexels-photo-3184405.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <p>Accounting & Taxation</p>
-            <h1>Mary Jones</h1>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="Second slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://images.pexels.com/photos/3183197/pexels-photo-3183197.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
+        <Carousel.Item className="position-relative">
+          <img className="img-fluid" src={banner1} alt="First slide" />
+          <Carousel.Caption className="slider-caption">
+            <h1>
+              Helping make your
+              <br /> financial dreams come true
+            </h1>
             <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              Capitalise on low hanging fruit to identify a ballpark value added
+              activity <br /> to beta test. Override the digital divide with
+              additional clickthroughs.
             </p>
+
+            <div className="button-area">
+              <button className="btn btn-one">Read More</button>
+              <button className="btn btn-two">Get in Touch</button>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className="position-relative">
+          <img className="img-fluid" src={banner2} alt="Second slide" />
+
+          <Carousel.Caption className="slider-caption">
+            <h1>
+              I help Business <br /> innovate and grow
+            </h1>
+            <p>
+              With over 10 years of experience helping businesses to find <br />{" "}
+              comprehensive solutions .
+            </p>
+            <div className="button-area">
+              <button className="btn btn-one">Read More</button>
+              <button className="btn btn-two">Get in Touch</button>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className="position-relative">
+          <img className="img-fluid" src={banner3} alt="Third slide" />
+
+          <Carousel.Caption className="slider-caption">
+            <h1>
+              New Bussines Advisory
+              <br />
+              Services Provider
+            </h1>
+            <p>
+              Interactively procrastinate high-payoff content without
+              backward-compatible data.
+              <br /> Quickly cultivate optimal processes and tactical profit
+              architectures.
+            </p>
+            <div className="button-area">
+              <button className="btn btn-one">Read More</button>
+              <button className="btn btn-two">Get in Touch</button>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
